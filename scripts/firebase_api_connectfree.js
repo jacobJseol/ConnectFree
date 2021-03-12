b4d7730efed9b935f58b5e2b665a2dc8bbbed618
Jacob Seol <jacobseol96@gmail.com>
2021-03-12 15:00:14 -0800

    Made changes to the nav bar of all html pages and added authentication/firebase and firestore to the website.

---
+++ scripts/firebase_api_connectfree.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD5TQlZU60d0dunIagU9md9oCNvZK42v2Q",
+    authDomain: "connectfree-fe03c.firebaseapp.com",
+    projectId: "connectfree-fe03c",
+    storageBucket: "connectfree-fe03c.appspot.com",
+    messagingSenderId: "286722217060",
+    appId: "1:286722217060:web:b829e8203eb4d3c4f9fb71"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
