73463035c83e80c3db2d3e46d9f4c1329ecb552e
Jacob Seol <jacobseol96@gmail.com>
2021-03-26 11:08:52 -0700

    Added a new script and modified main.html to recognize users

---
+++ scripts/functions.js
@@ -0,0 +1,21 @@
+function sayHello() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            // Do something for the user here. 
+            console.log(user.uid);
+            db.collection("users").doc(user.uid)
+                .get()
+                .then(function (doc) {
+                    var n = doc.data().name;
+                    console.log(n);
+                    //using jquery
+                    $("#username").text(n);
+                    //using vanilla javascript
+                    //document.getElementById("username").innerText = n;
+                })
+        } else {
+            // No user is signed in.
+        }
+    });
+}
