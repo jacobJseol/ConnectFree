a5d3876e84eaa539489f9d65adca2996a9206425
Jacob Seol <jacobseol96@gmail.com>
2021-04-15 23:12:26 -0700

    Created functionality for form submission to read and write data to Firestore.

--- scripts/functions.js
+++ scripts/functions.js
@@ -88,27 +88,27 @@ function writeLessons() {
 
 function servicesQuery() {
     db.collection("services")
-            .get()
-            .then(function (snap) {
-                snap.forEach(function (doc) {
-                    console.log(doc.data());
-                    var title = doc.data().name;    //key "name"
-                    var pic = doc.data().picture;   //key "picture"
-                    var desc = doc.data().description;
-                    var person = doc.data().host;
+        .get()
+        .then(function (snap) {
+            snap.forEach(function (doc) {
+                console.log(doc.data());
+                var title = doc.data().name; //key "name"
+                var pic = doc.data().picture; //key "picture"
+                var desc = doc.data().description;
+                var person = doc.data().host;
 
-                    // construct the string for card
-                    var codestring = '<br />' + '<div class="card">' +
-                        '<img src="images/' + pic + '" class="card-img-top">' +
-                        '<div class="card-body">' +
-                        '<h5 class="card-title">' + title + '</h5>' +
-                        '<p class="card-text">' + desc + '</p>' +
-                        '<p class="card-text"><small class="text-muted">Hosted by '+ person + '</small></p>' +
-                        '</div>';
-                    // append with jquery to DOM
-                    $("#services-go-here").prepend(codestring);
-                })
+                // construct the string for card
+                var codestring = '<br />' + '<div class="card">' +
+                    '<img src="images/' + pic + '" class="card-img-top">' +
+                    '<div class="card-body">' +
+                    '<h5 class="card-title">' + title + '</h5>' +
+                    '<p class="card-text">' + desc + '</p>' +
+                    '<p class="card-text"><small class="text-muted">Hosted by ' + person + '</small></p>' +
+                    '</div>';
+                // append with jquery to DOM
+                $("#services-go-here").prepend(codestring);
             })
+        })
 }
 //servicesQuery();
 
@@ -124,8 +124,8 @@ function getSearch() {
             .then(function (snap) {
                 snap.forEach(function (doc) {
                     console.log(doc.data());
-                    var title = doc.data().name;    //key "name"
-                    var pic = doc.data().picture;   //key "picture"
+                    var title = doc.data().name; //key "name"
+                    var pic = doc.data().picture; //key "picture"
                     var desc = doc.data().description;
                     var person = doc.data().host;
 
@@ -145,27 +145,57 @@ function getSearch() {
 }
 //getSearch();
 
+function submit() {
+    document.getElementById("submission").addEventListener('click', function () {
+        var aboutMe = document.getElementById("about").value;
+        console.log(aboutMe);
+        var myServices = document.getElementById("your-services").value;
+        console.log(myServices);
+
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                // User is signed in.
+                // Do something for the user here. 
+                console.log(user.uid);
+                db.collection("users").doc(user.uid)
+                    .get()
+                    .then(function (doc) {
+                        var usersRef = db.collection("users").doc(user.uid);
+                        usersRef.set({
+                            about: aboutMe,
+                            mystuff: myServices
+                        }, {merge: true
+                        })
+                    })
+            } else {
+                // No user is signed in.
+            }
+        });
+
+    })
+}
+//submit();
+
 function getLessons() {
-        db.collection("lessons")
-            //.limit(2)
-            //.orderBy("price")
-            //.orderBy("price", "desc")
-            .get()
-            .then(function (snap) {
-                snap.forEach(function (doc) {
-                    var num = doc.data().number;
-                    var pic = doc.data().picture;
-                    var title = doc.data().name;
+    db.collection("lessons")
+        //.limit(2)
+        //.orderBy("price")
+        //.orderBy("price", "desc")
+        .get()
+        .then(function (snap) {
+            snap.forEach(function (doc) {
+                var num = doc.data().number;
+                var pic = doc.data().picture;
+                var title = doc.data().name;
 
-                    var newdom = '<div class="card">' + '<div class="card-header">'
-                        + '<b>Lesson Number</b>' + num + '</div><div class="card-body">'
-                        + '<img src="images/' + pic + '"alt="..." class="img-thumbnail">'
-                        + title + '</div>';
+                var newdom = '<div class="card">' + '<div class="card-header">' +
+                    '<b>Lesson Number</b>' + num + '</div><div class="card-body">' +
+                    '<img src="images/' + pic + '"alt="..." class="img-thumbnail">' +
+                    title + '</div>';
 
-                    $("#lessons-go-here").prepend(newdom);
-                    
-                })
+                $("#lessons-go-here").prepend(newdom);
+
             })
-    }
-
+        })
+}
 //getLessons();
